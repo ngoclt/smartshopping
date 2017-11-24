28f9eed30d9d5975e3f31ba5a4ff369d0ac62348
Ngoc LE <ngoc.le@smarp.com>
2017-11-24 19:31:19 +0200

    UI for main page.

--- SmartShopping/ViewControllers/MainViewController.swift
+++ SmartShopping/ViewControllers/MainViewController.swift
@@ -20,7 +20,22 @@ class MainViewController: BaseViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension MainViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StoreTableViewCell", for: indexPath)
+        return cell
+    }
+}
+
+extension MainViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
--- SmartShopping/Views/StoreTableViewCell.swift
+++ SmartShopping/Views/StoreTableViewCell.swift
@@ -11,9 +11,52 @@ import Material
 
 class StoreTableViewCell: TableViewCell {
     
+    @IBOutlet fileprivate var lblName: UILabel! {
+        didSet {
+            lblName.textColor = Color.orange.base
+        }
+    }
+    
+    @IBOutlet fileprivate var lblAddress: UILabel! {
+        didSet {
+            
+        }
+    }
+    
+    @IBOutlet fileprivate var ivThumb: UIImageView! {
+        didSet {
+            
+        }
+    }
+    
+    @IBOutlet fileprivate var btnFavorite: FlatButton! {
+        didSet {
+            btnFavorite.setImage(Icon.favorite?.tint(with: Color.red.base), for: .normal)
+        }
+    }
+    
+    @IBOutlet fileprivate var btnShare: FlatButton! {
+        didSet {
+            btnShare.setImage(Icon.share?.tint(with: Color.blue.base), for: .normal)
+        }
+    }
+    
+    @IBOutlet fileprivate var lblOpeningTime: UILabel! {
+        didSet {
+            lblOpeningTime.textColor = Color.lightGreen.base
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
     
+    @IBAction fileprivate func didTapBTNFavorite(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction fileprivate func didTapBTNShare(_ sender: UIButton) {
+        
+    }
 }
 
