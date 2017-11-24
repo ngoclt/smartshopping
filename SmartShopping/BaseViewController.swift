8631eeb45046d19bde55b3bf6bbdb60552967d56
Ngoc LE <ngoc.le@smarp.com>
2017-11-24 10:00:09 +0200

    Initial Commit

---
+++ SmartShopping/BaseViewController.swift
@@ -0,0 +1,25 @@
+//
+//  BaseViewController.swift
+//  SmartShopping
+//
+//  Created by Ngoc LE on 11/24/17.
+//  Copyright © 2017 Ngoc LE. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
---
+++ SmartShopping/NotificationViewController.swift
@@ -0,0 +1,24 @@
+//
+//  NotificationViewController.swift
+//  SmartShopping
+//
+//  Created by Ngoc LE on 11/24/17.
+//  Copyright © 2017 Ngoc LE. All rights reserved.
+//
+
+import UIKit
+
+class NotificationViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
